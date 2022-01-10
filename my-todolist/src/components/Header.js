c9d1f3a02478dd6067eb139db180b91e19e2537e
kabirsingh30 <kabir@optiphoenix.com>
2022-01-10 17:53:13 +0530

    routes done

--- my-todolist/src/components/Header.js
+++ my-todolist/src/components/Header.js
@@ -1,29 +1,27 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom'
 
 function Header() {
     return (
         <>
-        <Router>
             <div className="header_main_wrapper">
                 <div className="header_main_left_menu">
 
                 </div>
                 <div className="header_main_right_menu">
                         <span className="header_todoList">
-                            <Link to="/todolist">
+                            <NavLink to="/todolist">
                                 <img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/50/000000/external-list-support-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"/>
-                            </Link>
+                            </NavLink>
                         </span>
                     
                     <span className="header_account">
-                        <Link to="/account">
+                        <NavLink to="/account">
                             <img src="https://img.icons8.com/ios-filled/40/000000/guest-male--v1.png"/>
-                        </Link>
+                        </NavLink>
                     </span>
                     
                 </div>
             </div>
-        </Router>
         </>
     );
 }
